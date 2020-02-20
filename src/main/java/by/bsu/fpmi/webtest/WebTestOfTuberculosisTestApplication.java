585cc3180d2f0bc7b5422538c5dd3cc47369ef33
svku0919 <svkulich@gmail.com>
2020-02-20 09:56:09 +0300

    initial commit

---
+++ src/main/java/by/bsu/fpmi/webtest/WebTestOfTuberculosisTestApplication.java
@@ -0,0 +1,13 @@
+package by.bsu.fpmi.webtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTestOfTuberculosisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebTestOfTuberculosisTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/bsu/fpmi/webtest/WebTestOfTuberculosisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsu.fpmi.webtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTestOfTuberculosisTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
